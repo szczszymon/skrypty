0b1187e03ba715f01ae7bb606c9a44b78ab178d1
Szymon Szczerbiński <szymonszcz@student.agh.edu.pl>
2022-12-19 16:49:59 +0100

    Lab10 - added JSDoc documentation

---
+++ Lab10/module.js
@@ -0,0 +1,42 @@
+/**
+ * A class representing a mathematical operation.
+ * @class
+ */
+class Operation {
+    /**
+     * Creates an instance of Operation.
+     * @constructor
+     * @param {number} x - The first operand.
+     * @param {number} y - The second operand.
+     */
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Calculates the sum of the two operands.
+     * @returns {number} The sum of the operands.
+     */
+    sum() {
+        return this.x + this.y;
+    }
+}
+
+/**
+ * Exports the Operation class.
+ * @module Operation
+ */
+//export { Operation };
+
+/**
+ * The default export for the Operation module.
+ * @default
+ */
+//export default Operation;
+
+/**
+ * Exports the Operation class.
+ * @module Operation
+ */
+module.exports = Operation;
